10a5a13511321db6cd45698c57f20ba7f39786ec
DiZ73 <dkroell@acm.org>
2015-10-28 21:14:40 -0400

    Adding queue implementation to the script, for memory management.

--- map_app/app.js
+++ map_app/app.js
@@ -1,21 +1,40 @@
+/**
+ * Map that helps visualize obsticle location using Open Street Map as a
+ * dependency for the application. Obsticle updates, and creations are
+ * specified in methods, and functions.
+ *
+ *
+ * @author dkroell
+ * @version 10/26/2015
+ *
+ */
+
 map = new OpenLayers.Map("mapdiv");
 map.addLayer(new OpenLayers.Layer.OSM());
+
 
     var zoom=17;
 
     //The vector layer
     var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
 
+
+
+
     //Make the feature a plain OpenLayers marker
     var feature = new OpenLayers.Feature.Vector(
 	new OpenLayers.Geometry.Point(-76.427991, 38.144616).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()),
         {description:'X-8 Plane for AUVSI SUAS Competition'} ,
-        {externalGraphic: '/map_app/img/star_plane.png', graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25  }
-        //{externalGraphic: '/map_app/img/track_pixel.png', graphicHeight: 10, graphicWidth: 10, graphicXOffset:-4, graphicYOffset:-13} * This is just for georss accuracy *
+        { externalGraphic: 'map_app/img/star_plane.png', 
+          graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25 }
     );
-                     
-    vectorLayer.addFeatures(feature);
-    map.addLayer(vectorLayer);
+
+
+    //Add the Plane's Icon automatically to the center of the runway.
+            vectorLayer.addFeatures(feature);
+            map.addLayer(vectorLayer);
+
+
 
 // Where to position the map.
 var lonLat = new OpenLayers.LonLat( -76.427991,38.144616 )                     
@@ -25,30 +44,44 @@ var lonLat = new OpenLayers.LonLat( -76.427991,38.144616 )
              );
 map.setCenter (lonLat, zoom);
 
+
+
+
+
+//queue implementation for tracer display. To avoid overload.
+var queue_plane = [];
+
+
+
+
 //US Naval Electronic systems center  LonLat: (-76.427991, 38.144616)
-function changePlaneLoc(lon, lat) {
-    // feature.style.externalGraphic = '/map_app/img/track_pixel.png';
+function changePlaneLoc(long, lat) {
+
+    if (queue_plane.length >= 45) {
+        vectorLayer.removeFeatures(queue_plane[0]);
+        queue_plane.shift();
+    }
+
+    feature.style.externalGraphic = 'map_app/img/track_pixel.png';
     feature.style.graphicHeight = 10;
     feature.style.graphicWidth = 10;
     feature.style.graphicXOffset = -4;
     feature.style.graphicYOffset = -13;
 
     vectorLayer.addFeatures(feature);
+    queue_plane.push(feature);
+
     feature = new OpenLayers.Feature.Vector(
-        new OpenLayers.Geometry.Point(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()),
+        new OpenLayers.Geometry.Point(long, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()),
         {description:'X-8 Plane for AUVSI Competition'} ,
-        {externalGraphic:'/map_app/img/star_plane.png', graphicHeight: 30, graphicWidth: 29, graphicXOffset:-12, graphicYOffset:-25 }
+        {externalGraphic:'map_app/img/star_plane.png', graphicHeight: 30, graphicWidth: 29, graphicXOffset:-12, graphicYOffset:-25 }
     );
     vectorLayer.addFeatures(feature);
     UpdateLayer(vectorLayer);
-
-    var lonLat = new OpenLayers.LonLat( lon, lat )                     
-            .transform(                                                             
-                  new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984  
-                  map.getProjectionObject() // to Spherical Mercator Projection       
-             );
-    map.panTo(lonLat);
 }
+
+
+
 
 // function to automatically update the lay maps when a marker changes position.
 function UpdateLayer(layer) {
@@ -63,51 +96,78 @@ function UpdateLayer(layer) {
 
 
 
+
+
 // This creates an Object which will appear on the map.
 function createStationaryObsticle(long, lat) {
     obst = new OpenLayers.Feature.Vector(
 		      new OpenLayers.Geometry.Point( long, lat ).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()),
 		      {description: 'Stationary Object'},
-		      {externalGraphic:'/map_app/img/cylinder_obst.png', graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25}
+		      {externalGraphic:'map_app/img/cylinder_obst.png', graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25}
 					 );
     vectorLayer.addFeatures(obst);
 }
 
-//creating an array of moving obsticles
-var arrayObstMov = [];
 
+
+//Creating an array of moving obsticles
+var objectObstMov = {
+    Obsticles:[]
+};
+
+
+
+// Creates a Moving obsticle on the map
 function createMovingObsticle(long, lat, id) {
     obst_mov = new OpenLayers.Feature.Vector(
 		      new OpenLayers.Geometry.Point( long, lat ).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()),
 		      {description: id},
-	     	      {externalGraphic:'/map_app/img/sphere_obst.png', graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25}
+	     	      {externalGraphic:'map_app/img/sphere_obst.png', graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25}
     );
-    arrayObstMov.push(obst_mov);
+
+
+
+    //Creating JSON object to push to the Array
+    var obst_object = {
+        "Obsticle": obst_mov,
+        "identification" : id,
+         "obsticleLocation":[]
+    };
+
+    objectObstMov.Obsticles.push(obst_object);
     vectorLayer.addFeatures(obst_mov);
 }
 
-//function to change the location of a moving obsticle.
+
+//function to change the location of a moving obsticle which has already been created.
 function changeMovingObsticleLoc(long, lat, id) {
     var curr;
-    for (var i = 0; i < arrayObstMov.length; i++) {
-	if (arrayObstMov[i].attributes.description == id) {
-	    curr = arrayObstMov[i];
-	    
-	    curr.style.externalGraphic = '/map_app/img/track_pixel_obst.png';
-	    curr.style.graphicHeight = 10;
-	    curr.style.graphicWidth = 10;
-	    curr.style.graphicXOffset = -4;
-	    curr.style.graphicYOffset = -13;
+    for (var i = 0; i < objectObstMov.Obsticles.length; i++) {
+	if (objectObstMov.Obsticles[i].identification == id) {
 
-	    vectorLayer.addFeatures(curr);
+        objectObstMov.Obsticles[i].Obsticle.style.externalGraphic = 'map_app/img/track_pixel_obst.png';
+        objectObstMov.Obsticles[i].Obsticle.style.graphicHeight = 10;
+        objectObstMov.Obsticles[i].Obsticle.style.graphicWidth = 10;
+        objectObstMov.Obsticles[i].Obsticle.style.graphicXOffset = -4;
+        objectObstMov.Obsticles[i].Obsticle.style.graphicYOffest = -10;
+        
+        objectObstMov.Obsticles[i].obsticleLocation.push(objectObstMov.Obsticles[i].Obsticle);
+        vectorLayer.addFeatures(objectObstMov.Obsticles[i].Obsticle);
+
 	    curr = new OpenLayers.Feature.Vector(
 		    new OpenLayers.Geometry.Point(long, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()),
  		    {description: id} ,
-		    {externalGraphic:'/map_app/img/sphere_obst.png', graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25 }
+		    {externalGraphic:'map_app/img/sphere_obst.png', graphicHeight: 30, graphicWidth: 30, graphicXOffset:-12, graphicYOffset:-25 }
 		    );
+
+        objectObstMov.Obsticles[i].Obsticle = curr;
 	    vectorLayer.addFeatures(curr);
 	    UpdateLayer(vectorLayer);
-	}
     }
-}
+    while (objectObstMov.Obsticles[i].obsticleLocation.length >= 45) {
+        vectorLayer.removeFeatures(objectObstMov.Obsticles[i].obsticleLocation[0]);
+        objectObstMov.Obsticles[i].obsticleLocation.shift();
+    }
 
+  }
+}
