23f3c69c0ec2ac345d9f908dec4d679bd0983c1f
Legonigel <legonigel@gmail.com>
2016-03-17 18:33:49 -0400

    Fix spelling issues in api.js

--- api.js
+++ api.js
@@ -3,111 +3,111 @@
  */
 
 var libmath = require('./libmath.js');
-var grid 	= require('./grid.js');
-var config 	= require('./config.js');
+var grid  = require('./grid.js');
+var config  = require('./config.js');
 
 var api = {
 
-	/* Get current grid information for DROPS.
-	 *
-	 * @param waypoints 	Object 	structure of waypoint data
-	 * @return JSON: data that provides X and Y parameters for the Search area.
-	 */
-	get_grid_details: function(telemetry, waypoints) {
+  /* Get current grid information for DROPS.
+   *
+   * @param waypoints   Object  structure of waypoint data
+   * @return JSON: data that provides X and Y parameters for the Search area.
+   */
+  get_grid_details: function(telemetry, waypoints) {
 
-		var prevWaypoint = waypoints.get_last_waypoint();
-		var nextWaypoint = waypoints.get_next_waypoint()
-		var followingWaypoint = waypoints.get_following_waypoint();
+    var prevWaypoint = waypoints.get_last_waypoint();
+    var nextWaypoint = waypoints.get_next_waypoint();
+    var followingWaypoint = waypoints.get_following_waypoint();
 
-		if(!prevWaypoint || !nextWaypoint || !followingWaypoint) {
-			return {
-				"grid_width": 100,
-				"grid_height": 400,
-				"is_changed": false,
-				"obstacles": {
-					"moving_obstacles": [
-						{
-							"x": 50,
-							"y": 200,
-							"radius": 10,
-							"heading": 0,
-							"velocity": 5,
-							"is_changed": false
-						}
-					],
-					"stationary_obstacles": [
-						{
-							"x": 30,
-							"y": 20,
-							"radius": 15
-						}
-					]
-				},
-				"goal": {
-					"x": 50,
-					"y": 350,
-					"theta": 15
-				},
-				"location": {
-					"x": 50,
-					"y": 5,
-					"theta": 350
-				}
-			};
-		}
+    if(!prevWaypoint || !nextWaypoint || !followingWaypoint) {
+      return {
+        "grid_width": 100,
+        "grid_height": 400,
+        "is_changed": false,
+        "obstacles": {
+          "moving_obstacles": [
+            {
+              "x": 50,
+              "y": 200,
+              "radius": 10,
+              "heading": 0,
+              "velocity": 5,
+              "is_changed": false
+            }
+          ],
+          "stationary_obstacles": [
+            {
+              "x": 30,
+              "y": 20,
+              "radius": 15
+            }
+          ]
+        },
+        "goal": {
+          "x": 50,
+          "y": 350,
+          "theta": 15
+        },
+        "location": {
+          "x": 50,
+          "y": 5,
+          "theta": 350
+        }
+      };
+    }
 
-		grid.set_width(config.get_config('grid').grid_with);
+    grid.set_width(config.get_config('grid').grid_width);
 
-		// calculate distance from last waypoint to next
-		// waypoint padding also added to grid begin / end
-		if(prevWaypoint && nextWaypoint) {
-			
-			grid.set_height(
-				grid.set_goal_lon(libmath.get_distance(prevWaypoint, nextWaypoint) 
-					+ (config.get_config('grid').grid_padding_height)
-				)
-			);
-			
-			grid.get_grid().grid_height += (config.get_config('grid').grid_padding_height);
+    // calculate distance from last waypoint to next
+    // waypoint padding also added to grid begin / end
+    if(prevWaypoint && nextWaypoint) {
 
-			grid.set_goal_lat(config.get_config('grid').grid_with / 2);
+      grid.set_height(
+        grid.set_goal_lon(libmath.get_distance(prevWaypoint, nextWaypoint)
+          + (config.get_config('grid').grid_padding_height)
+        )
+      );
 
-		}
+      grid.get_grid().grid_height += (config.get_config('grid').grid_padding_height);
 
-		if(followingWaypoint) {
+      grid.set_goal_lat(config.get_config('grid').grid_width / 2);
 
-			var theta_wptLast_wptNext = libmath.get_bearing(prevWaypoint, nextWaypoint);
-			var theta_wptNext_wptFollowing = libmath.get_bearing(nextWaypoint, followingWaypoint);
+    }
 
-			grid.set_goal_theta(theta_wptNext_wptFollowing - theta_wptLast_wptNext);
-		}
+    if(followingWaypoint) {
 
-		// calculate plane location
-		var location = libmath.get_distance_from_path(telemetry.get_coords(), prevWaypoint, nextWaypoint);
-		var theta = telemetry.get_heading() - libmath.get_bearing(prevWaypoint, followingWaypoint);
+      var theta_wptLast_wptNext = libmath.get_bearing(prevWaypoint, nextWaypoint);
+      var theta_wptNext_wptFollowing = libmath.get_bearing(nextWaypoint, followingWaypoint);
 
-		if(theta < 360) {
-			theta += 360;
-		}
+      grid.set_goal_theta(theta_wptNext_wptFollowing - theta_wptLast_wptNext);
+    }
 
-		grid.set_location_lat(location.x + (config.get_config('grid').grid_with / 2));
-		grid.set_location_lon(location.y + config.get_config('grid').grid_padding_height);
-		grid.set_location_theta(theta);
+    // calculate plane location
+    var location = libmath.get_distance_from_path(telemetry.get_coords(), prevWaypoint, nextWaypoint);
+    var theta = telemetry.get_heading() - libmath.get_bearing(prevWaypoint, followingWaypoint);
 
-		return grid.get_grid();
-	},
+    if(theta < 360) {
+      theta += 360;
+    }
 
-	/**
-	 * @put /api/grid
-	 * Get the current search grid information from the Interop server.
-	 *
-	 * @param followingWaypoint 	Object 	the waypoint after the nextWaypoint
-	 * @return JSON: data that provides X and Y parameters for the Search area.
-	 */
-	put_path: function() {
-		// TODO
-		return 0;
-	}
+    grid.set_location_lat(location.x + (config.get_config('grid').grid_width / 2));
+    grid.set_location_lon(location.y + config.get_config('grid').grid_padding_height);
+    grid.set_location_theta(theta);
+
+    return grid.get_grid();
+  },
+
+  /**
+   * @put /api/grid
+   * Get the current search grid information from the Interop server.
+   *
+   * @param followingWaypoint   Object  the waypoint after the nextWaypoint
+   * @return JSON: data that provides X and Y parameters for the Search area.
+   */
+  put_path: function() {
+    // TODO
+    return 0;
+  }
 
 };
 
