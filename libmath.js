228aba0e6cc871a39fef56e2ee892407a9981c00
Nigel Armstrong <legonigel@gmail.com>
2016-03-20 14:53:22 -0400

    Libmath Fixes
     - Fixes for longitue scale in calculating distance
     - Fixes for calculating distance from path
     - Fixes get angle parameters
     - Fixes get_angle3 return to degrees

--- libmath.js
+++ libmath.js
@@ -9,7 +9,7 @@ var DEG_TO_RAD = 0.017453292519943295769236907684886;
 var libmath = {
 
 	constrain_float: function(amount, low, high) {
-		
+
 		if(isNaN(amount)) {
 			return (low + high) * 0.5;
 		}
@@ -22,16 +22,16 @@ var libmath = {
 	 * Location = latlon
 	 */
 	longitude_scale: function(waypoint) {
-		
+
 		var scale = Math.cos(waypoint.x * DEG_TO_RAD);
-    	return libmath.constrain_float(scale, 0.01, 1.0);
-	
+			return libmath.constrain_float(scale, 0.01, 1.0);
+
 	},
 
 	/**
 	 * Calculates distance between two vectors (meters).
 	 * Function taken from ArduPilot's "location.c" AP_Math library
-	 * https://github.com/diydrones/ardupilot/blob/master/libraries/AP_Math/location.cpp 
+	 * https://github.com/diydrones/ardupilot/blob/master/libraries/AP_Math/location.cpp
 	 * @return distance in meters
 	 */
 	get_distance: function(waypointA, waypointB) {
@@ -47,6 +47,7 @@ var libmath = {
 			waypoint2[i] = waypointB[i];
 		}
 
+		//Convert waypoint to deg * 1e7 for math
 		waypoint1.x *= 1e7;
 		waypoint1.y *= 1e7;
 
@@ -54,28 +55,45 @@ var libmath = {
 		waypoint2.y *= 1e7;
 
 		var delta_lat = waypoint2.x - waypoint1.x;
-		var delta_lon = (waypoint2.y - waypoint1.y) * libmath.longitude_scale(waypoint2);
+		var delta_lon = (waypoint2.y - waypoint1.y) * libmath.longitude_scale(waypointA);
 
 		return (Math.sqrt(Math.pow(delta_lat, 2) + Math.pow(delta_lon, 2))) * LOCATION_SCALING_FACTOR;
 
 	},
 
 	/**
-	 * Returns distance from waypointA to the line between waypointB and waypointC
+	 * Returns distance from location to the line between waypointA and waypointB
 	 * Assumes waypoints contain x (latitude) and y (longitude) properties in decimal notation.
 	 *
-	 * @param waypointA 	Object 	plane
-	 * @param waypointB 	Object 	previosu waypoint
-	 * @param waypointC 	Object 	next waypoint
+	 * @param location		Object	plane
+	 * @param waypointA		Object	previosu waypoint
+	 * @param waypointB		Object	next waypoint
 	 *
-	 * @return 	Object 	with properties { x: distance in meters perpendicular to path between waypointB and waypointC,
-	 * 										y: distance in meters parallel to path between waypointB and waypointC }
+	 * @return	Object	with properties { x: distance in meters perpendicular to path between waypointA and waypointB,
+	 *										y: distance in meters parallel to path between waypointA and waypointB }
 	 */
-	get_distance_from_path: function(waypointA, waypointB, waypointC) {
+	get_distance_from_path: function(location, waypointA, waypointB) {
 
+		var location1 = {};
 		var waypoint1 = {};
 		var waypoint2 = {};
-		var waypoint3 = {};
+
+		for(var i in location) {
+
+			location1[i] = location[i];
+
+			if(i == 'y' || i == 'x') {
+
+				location1[i] *= 1e7;
+
+				if(i == 'y') {
+					location1[i] *= libmath.longitude_scale(location);
+				}
+
+				location1[i] *= LOCATION_SCALING_FACTOR;
+
+			}
+		}
 
 		for(var i in waypointA) {
 
@@ -86,7 +104,7 @@ var libmath = {
 				waypoint1[i] *= 1e7;
 
 				if(i == 'y') {
-					waypoint1[i] *= libmath.longitude_scale(waypoint1);
+					waypoint1[i] *= libmath.longitude_scale(waypointA);
 				}
 
 				waypoint1[i] *= LOCATION_SCALING_FACTOR;
@@ -103,7 +121,7 @@ var libmath = {
 				waypoint2[i] *= 1e7;
 
 				if(i == 'y') {
-					waypoint2[i] *= libmath.longitude_scale(waypoint2);
+					waypoint2[i] *= libmath.longitude_scale(waypointB);
 				}
 
 				waypoint2[i] *= LOCATION_SCALING_FACTOR;
@@ -111,29 +129,12 @@ var libmath = {
 			}
 		}
 
-		for(var i in waypointC) {
+		var location_waypoint1 = { x: waypoint1.x - location1.x, y: waypoint1.y - location1.y };
+		var waypoint_delta = { x: waypoint1.x - waypoint2.x, y: waypoint1.y - waypoint2.y };
 
-			waypoint3[i] = waypointC[i];
-
-			if(i == 'y' || i == 'x') {
-
-				waypoint3[i] *= 1e7;
-
-				if(i == 'y') {
-					waypoint3[i] *= libmath.longitude_scale(waypoint3);
-				}
-
-				waypoint3[i] *= LOCATION_SCALING_FACTOR;
-
-			}
-		}
-
-		var waypoint12 = { x: waypoint2.x - waypoint1.x, y: waypoint2.y - waypoint1.y };
-		var waypoint23 = { x: waypoint2.x - waypoint3.x, y: waypoint2.y - waypoint3.y };
-
-		var dot = (waypoint12.x * waypoint23.x) + (waypoint12.y * waypoint23.y);
-		var cross = (waypoint12.x * waypoint23.y) - (waypoint12.y * waypoint23.x);
-		var dist = Math.sqrt(Math.pow(waypoint3.x - waypoint2.x, 2) + Math.pow(waypoint3.y - waypoint2.y, 2));
+		var dot = (location_waypoint1.x * waypoint_delta.x) + (location_waypoint1.y * waypoint_delta.y);
+		var cross = (location_waypoint1.x * waypoint_delta.y) - (location_waypoint1.y * waypoint_delta.x);
+		var dist = Math.sqrt(Math.pow(waypoint_delta.x, 2) + Math.pow(waypoint_delta.y, 2));
 
 		return {
 			x: cross / dist,
@@ -164,7 +165,7 @@ var libmath = {
 	/**
 	 * returns angle between two vectors in degrees
 	 */
-	get_angle: function(waypoint1, waypoint2, waypoint3) {
+	get_angle: function(waypoint1, waypoint2) {
 
 		var dot = (waypoint1.y * waypoint2.y) + (waypoint1.x * waypoint2.x);
 		var det = (waypoint1.y * waypoint2.x) - (waypoint1.x * waypoint2.y);
@@ -181,7 +182,7 @@ var libmath = {
 	 * @param waypoint3 point relative to waypoint3
 	 */
 	get_angle3: function(waypoint1, waypoint2, waypoint3) {
-	
+
 		var waypoint12 = { x: waypoint2.x - waypoint1.x, y: waypoint2.y - waypoint1.y };
 		var waypoint23 = { x: waypoint2.x - waypoint3.x, y: waypoint2.y - waypoint3.y };
 
@@ -190,7 +191,7 @@ var libmath = {
 		var alpha = Math.atan2(cross, dot);
 
 		// Math.floor(alpha * 180 / Math.PI + 0.5)
-		return alpha;
+		return alpha * 180 / Math.PI;
 
 	}
 
